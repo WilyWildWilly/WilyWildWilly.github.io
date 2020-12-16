325c4f581624ad7c20a98a41cbe278877d3968dc
guglielmoCG <guglielmo@atnos.com>
2020-12-16 16:49:10 +0100

    fixing orbits

--- p5samples/sketch.js
+++ p5samples/sketch.js
@@ -51,8 +51,8 @@ function setup() {
   if (selectedSample === 0) {
     translateX = 0;
     translateY = 0;
-    particle = new Particle(createVector(width / 2, 30), 2, createVector(2, 0));
-    moon = new Particle(createVector(particle.pos.x, particle.pos.y + 13), 0.5, createVector(1, 0));
+    particle = new Particle(createVector(width / 2, 30), 2, createVector(2.3, 0));
+    moon = new Particle(createVector(particle.pos.x, particle.pos.y + 13), 0.5, createVector(1.3, 0));
     attractor = new Attractor(width / 2, height / 2);
   }
   // tracker sample
